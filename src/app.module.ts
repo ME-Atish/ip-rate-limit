1028c029041b1abc9047901c4a056706e5212b9b
ME.AT <meatish2008@gmail.com>
2025-09-15 15:23:37 +0330

    feat: setup typorm for ip-tracker feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { IpTrackerController } from './modules/ip-tracker/ip-tracker.controller';
-import { IpTrackerService } from './modules/ip-tracker/ip-tracker.service';
 import { IpTrackerModule } from './modules/ip-tracker/ip-tracker.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppDataSource } from './database/data-source';
 
 @Module({
   imports: [TypeOrmModule.forRoot(AppDataSource.options), IpTrackerModule],
-  controllers: [IpTrackerController],
-  providers: [IpTrackerService],
 })
 export class AppModule {}
--- src/modules/ip-tracker/ip-tracker.module.ts
+++ src/modules/ip-tracker/ip-tracker.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { IpRecord } from './ip-tracker.entity';
+import { IpTrackerController } from './ip-tracker.controller';
+import { IpTrackerService } from './ip-tracker.service';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([IpRecord])],
+  controllers: [IpTrackerController],
+  providers: [IpTrackerService],
+})
 export class IpTrackerModule {}
